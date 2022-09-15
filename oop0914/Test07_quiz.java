d1707d5b7351a5d464384732ccba67a5641c0207
plzleavemealone <110089762+plzleavemealone@users.noreply.github.com>
2022-09-15 09:57:29 +0900

    Update Test07_quiz.java

--- oop0914/Test07_quiz.java
+++ oop0914/Test07_quiz.java
@@ -8,18 +8,30 @@ public class Test07_quiz {
 		//문1)주민번호의 각 숫자의 합을 구하시오 (Character클래스와 charAt()함수 이용)
         //   8+9+1+2+3+0+1+2+3+4+5+6+7
         String jumin="8912301234567";
-        char c = ' ';
+        
+        char a;
         int hap=0;
         for(int i=0; i<jumin.length(); i++) {
-        	c=jumin.charAt(i);
-        	hap+=c;
+        	a=jumin.charAt(i);
+        	hap+=Character.getNumericValue(a);
         }
         System.out.println(hap);
         
         //문2)대소문자를 서로 바꿔서 출력하시오 (Character클래스와 charAt()함수 이용)
         //   gONE wITH tHE wIND
         String str="Gone With The Wind";
-
+        
+        char b;
+        for(int i=0; i<str.length(); i++) {
+        	b=str.charAt(i);
+        	if(b>=65 && b<=90) {
+        		b+=32;
+        	} else if(b>96 && b<123) {
+        		b-=32;
+        	}
+        	System.out.print(b);
+        }
+        System.out.println();
 	}
 
 }
